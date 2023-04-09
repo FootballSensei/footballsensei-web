4f4e5a43ea3b629fb716c4805e28d58add22ca4b
ella2222 <ella2222@users.noreply.github.com>
2023-04-09 21:24:08 +0300

    database connection

--- FootballSensei/FootballSensei/Data/ProjectContext.cs
+++ FootballSensei/FootballSensei/Data/ProjectContext.cs
@@ -8,21 +8,37 @@ namespace FootballSensei.Data
 {
     public class ProjectContext : DbContext
     {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+
         public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
         {
         }
 
-        public object Players { get; internal set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
-
-
-
-
-
             base.OnModelCreating(modelBuilder);
+            //one team has many players
+            modelBuilder.Entity<Team>()
+                .HasMany(t => t.Players)
+                .WithOne(p => p.Team)
+                .HasForeignKey(p => p.TeamId);
+            
+            //one
+
+            //one match has 1 home team
+            modelBuilder.Entity<Match>()
+                .HasOne(m => m.HomeTeam)
+                .WithMany(t => t.HomeMatches)
+                .HasForeignKey(m => m.HomeTeamId);
+
+            //one match has 1 away team
+            modelBuilder.Entity<Match>()
+                .HasOne(m => m.AwayTeam)
+                .WithMany(t => t.AwayMatches)
+                .HasForeignKey(m => m.AwayTeamId);
         }
 
     }
