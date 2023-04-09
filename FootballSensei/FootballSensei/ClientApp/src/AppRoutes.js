32d25e22e66614a5537f12f37ef16678aacb84c5
valentinvale <valionsemen@gmail.com>
2023-04-10 02:49:20 +0300

    added matches component

--- FootballSensei/FootballSensei/ClientApp/src/AppRoutes.js
+++ FootballSensei/FootballSensei/ClientApp/src/AppRoutes.js
@@ -1,6 +1,9 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Matches } from "./components/Matches";
+//import { Players } from "./components/Players";
+//import { Teams } from "./components/Teams";
 
 const AppRoutes = [
   {
@@ -14,7 +17,24 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+    },
+
+  {
+      path: '/matches',
+      element: <Matches />
+  }//,
+  /*
+  {
+      path: '/teams',
+      element: <Teams />
+  },
+
+  {
+      path: '/players',
+      element: <Players />
   }
+  */
+
 ];
 
 export default AppRoutes;
---
+++ FootballSensei/FootballSensei/ClientApp/src/components/Matches.js
@@ -0,0 +1,60 @@
+﻿import React, { Component } from 'react';
+
+export class Matches extends Component {
+    static displayName = Matches.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { matches: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateMatchesData();
+    }
+
+    static renderMatchesTable(matches) {
+        return (
+            <table className='table table-striped' aria-labelledby='tableLabel'>
+                <thead>
+                    <tr>
+                        <th>Home Team</th>
+                        <th>Away Team</th>
+                        <th>Match Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                    {matches.map(match =>
+                        <tr key={match.Id}>
+                            <td>{match.HomeTeamName}</td>
+                            <td>{match.AwayTeamName}</td>
+                            <td>{match.Date}</td>
+                        </tr>
+                    )}
+
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Matches.renderMatchesTable(this.state.matches);
+
+        return (
+            <div>
+                <h1 id='tableLabel'>Matches</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateMatchesData() {
+        const response = await fetch('matches');
+        const data = await response.json();
+        this.setState({ matches: data, loading: false });
+    }
+}
